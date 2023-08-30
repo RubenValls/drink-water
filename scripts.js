5a8e4f793ab1c2563201c8b4c44a1676ef6fef4d
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-30 10:42:34 +0200

    added the possibility to select some glasses of water

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 const glasses = document.querySelectorAll(".glass");
+let glassesOrder = 0;
 
 glasses.forEach((glass) => {
+    let order = glassesOrder;
     glass.addEventListener("click", () => {
-        if(glass.classList.contains("filled")){
-            glass.classList.remove("filled");
-        }else{
-            glass.classList.add("filled");
+        let orderToFollow = order
+        glasses.forEach((glass) => glass.classList.remove("filled"));
+        while(orderToFollow >= 0){
+            glasses[orderToFollow].classList.add("filled");
+            orderToFollow--
         }
     })
+    glassesOrder++;
 })
