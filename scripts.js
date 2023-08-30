2fc418fee10e17b5dab3ba8d8bc3db22d67b01cc
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-30 11:04:36 +0200

    added functionality filling the main glass

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 const glasses = document.querySelectorAll(".glass");
+const remainContainer = document.querySelector(".remained-container");
+const percentageContainer = document.querySelector(".percentage-container");
 
 glasses.forEach((glass, index) => {
     glass.addEventListener("click", () => {
         let orderToFollow = index
+        let percentage = ((index + 1)/glasses.length) * 100;
+        
         glasses.forEach((glass) => glass.classList.remove("filled"));
+        
         while(orderToFollow >= 0){
             glasses[orderToFollow].classList.add("filled");
-            orderToFollow--
+            orderToFollow--;
         }
+
+        percentageContainer.style.visibility = "visible";
+        remainContainer.style.height = `${100 - percentage}%`;
+        percentageContainer.style.height = `${percentage}%`;
     })
 })
